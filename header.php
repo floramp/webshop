5c71aff7b82e2a1c3218ec14292e5cef5dff01aa
FLORIANSPC\Florian <sehr-gute-sache@web.de>
2017-09-19 15:49:35 +0200

    header

--- header.php
+++ header.php
@@ -8,4 +8,26 @@
  * @version 1.0
  */
 
-?>
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//DE" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+<head profile="http://gmpg.org/xfn/11">
+
+  <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+  <title>Beratungsnavi</title>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); echo '?' . filemtime( get_stylesheet_directory() . '/style.css'); ?>" type="text/css" media="screen, projection" />
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<?php wp_head(); ?>
+</head>
+	
+<body>
+	<p>header</p>
+	
+	
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package Webshop
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div>
+	<p>Webshop</p>
+
+</div>
+	<?php
+/**
+ * The main template file
+ *
+ * @package Webshop
+ * @version 1.0
+ */
+
+get_footer(); ?>
+	
+</body>
+
+
